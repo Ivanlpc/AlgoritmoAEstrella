f9d3ba86d83b88ee45b81d12941c7f83b3c75163
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2022-05-16 00:09:07 +0200

    Update Laberinto.java

--- src/algoritmoLaberinto/Laberinto.java
+++ src/algoritmoLaberinto/Laberinto.java
@@ -18,25 +18,23 @@ private Nodo[][] generarLab(){
 	 e1.setSymbol('I');
 	 e2.setSymbol('O');
 	 e1.setH(e1.calcularH(e2)); //Calcula la H inicial
-Nodo [][] matriz=new Nodo[60][80]; //60 filas 80 columnas
+Nodo [][] matriz=new Nodo[Main.filas][Main.columnas]; //60 filas 80 columnas
 matriz[e1.getX()][e1.getY()]=e1;
 matriz[e2.getX()][e2.getY()]=e2;
-	for(int i=0;i<60;i++) {
-		for(int j=0;j<80;j++) {
-			Nodo n = new Nodo(i,j);
+	for(int i=0;i<Main.filas;i++) {
+		for(int j=0;j<Main.columnas;j++) {
+			Nodo n = new Nodo(i,j); //Crea el objeto Nodo y lo guarda dentro de la matriz
 			n.setX(i);
 			n.setY(j);
 			int numero=(int) (Math.random()*10);
 		if(numero>7 && !n.equals(e1) && !n.equals(e2)) {
-			n.setSymbol('*');
+			n.setSymbol('*'); //Si el objeto es un obstáculo tendrá ese símbolo
 			matriz[i][j]=n;
 		}else if(!n.equals(e1) && !n.equals(e2)){
-			n.setH(n.calcularH(e2));
+			n.setH(n.calcularH(e2)); //Calcula el valor de la función heurística de cada nodo a la hora de crear el laberinto
 
-			matriz[i][j]=n;
-		}else {
-			
-			}
+			matriz[i][j]=n; //Guarda el nodo creado en la matriz,se repite el proceso con cada coordenada de la matriz
+		}
 				}
 	}
 		return matriz;
@@ -48,7 +46,7 @@ public Nodo getNodo(int x, int y) {
 	return matriz[y][x];
 }
 public boolean esObstaculo(int x, int y) {
-	if(x<=59 && y<=79) {
+	if(x<Main.filas && y<Main.columnas) {
 	if(matriz[x][y].getSymbol()=='*') {
 		return true;
 	}else {
@@ -67,8 +65,8 @@ public Nodo getEstadoFinal() {
 
 public String toString() { 
     String salida = "";    
-    for (int x=0; x<60; x++) { 
-        for (int y=0; y<80; y++) { 
+    for (int x=0; x<Main.filas; x++) { 
+        for (int y=0; y<Main.columnas; y++) { 
         	
             salida += this.matriz[x][y].getSymbol(); 
         }
